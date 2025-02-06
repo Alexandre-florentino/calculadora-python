c5a5dd04eaa86213d86032301fc2248ad1d151c5
Alexandre Florentino <ajbflorentinojr@gmail.com>
2025-02-06 19:20:53 -0300

    correção do bug

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,7 @@
 while True:
     #Primeiro passo é definir as operações que a calculadora vai realizar.
     print("\nSelecione a operação:")
+    print ("1 - Adição")
     print("2 - Subtração")
     print("3 - Multiplicação")
     print("4 - Divisão")
